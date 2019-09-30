ae19b1178ef8bffa4d4273014ae79d0fdf963b72
Tolulope Oke <33998304+Limitless19@users.noreply.github.com>
2019-09-30 17:48:48 +0100

    .

--- app/src/main/java/com/limitless/smarthome/MainActivity.java
+++ app/src/main/java/com/limitless/smarthome/MainActivity.java
@@ -3,16 +3,30 @@ package com.limitless.smarthome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton fanImageButton,bulbImageButton,televisionImageButton,fridgeImageButton,pumpImageButton;
-
+    EditText editText;
+    Button speechButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fanImageButton = findViewById(R.id.fanImageButton);
+        bulbImageButton = findViewById(R.id.bulbImageButton);
+        televisionImageButton = findViewById(R.id.televisionImageButton);
+        fridgeImageButton = findViewById(R.id.fridgeImageButton);
+        pumpImageButton = findViewById(R.id.pumpImageButton);
+        editText = findViewById(R.id.editText);
+        speechButton = findViewById(R.id.speechButton);
+
+
+
     }
 }
